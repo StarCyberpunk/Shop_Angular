8726afc860a99a10415e2cd6c12aa05c26f141b3
Emil <Starostin.emil@mail.ru>
2023-09-20 07:37:57 +0300

    Modded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ProductComponent} from "./components/products/product.component";
+import {HeaderComponent} from "./components/header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,HeaderComponent,ProductComponent
   ],
   imports: [
     BrowserModule
